04d57d2d122982ba1e3bb2f0478adbf46562894d
hoopma <hoopma@yandex.ru>
2024-09-06 09:56:37 +0300

    fixing errors

--- web/start.sh
+++ web/start.sh
@@ -4,4 +4,4 @@ python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py createsuperuser --noinput --username=root --email=test@mail.ru
 
-exec gunicorn --bind 0.0.0.0:80 web.wsgi:application
+exec gunicorn --bind 0.0.0.0:8000 web.wsgi:application
